93a495b9d5a666e7fe537fafec3dfb8ae6cd22a8
odake <odake@weg.net>
2022-11-29 07:47:27 -0300

    commit angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,57 @@
 import { Component } from '@angular/core';
 
+import * as Stomp from 'stompjs';
+import * as SockJS from 'sockjs-client';
+import * as $ from "jquery";
+import * as Socket from 'socket.io-client';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  private serverUrl: string = 'http://localhost:8080/socket'
   title = 'angularSocket';
+  private stompClient: any;
+
+  constructor(){
+    this.initializeWebSocketConnection();
+  }
+
+  initializeWebSocketConnection(){
+    let ws = new SockJS(this.serverUrl);
+    this.stompClient = Stomp.over(ws);
+    let that = this;
+    
+    this.stompClient.connect({}, function(frame: any) {
+      that.stompClient.subscribe("/feedback", (message: any) => {
+        if(message.body) {
+          $(".chat").append("<div class='message'>"+/*JSON.parse(message.body)["cadastro"]*/message.body+"</div>")
+          console.log(message.body);
+        }
+      });
+    });
+
+    let ws2 = new SockJS(this.serverUrl);
+    let stompClient2 = Stomp.over(ws2);
+    stompClient2.connect({}, function(frame: any) {
+      stompClient2.subscribe("/feedback2", (message: any) => {
+        if(message.body) {
+          $(".chat").append("<div class='message'>"+message.body+"</div>")
+          console.log(message.body);
+        }
+      });
+    });
+  }
+
+  sendMessage(message: any){
+    this.stompClient.send("/tester/log" , {}, message);
+    $('#input').val('');
+  }
+
+  login(message: any){
+    this.stompClient.send("/tester/startSession" , {}, message);
+    $('#cadastro').val('');
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import * as SockJS from 'sockjs-client';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
